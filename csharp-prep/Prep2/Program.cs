5cee3ddfd3f8adcff928f3a0a9785b79c3c01c96
Judy Hanson <judy.l.hanson@gmail.com>
2024-09-19 20:14:19 -0700

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What's your grade percentage? ");
+        string studentGreadFromAns = Console.ReadLine();
+        int studentGrade = int.Parse(studentGreadFromAns);
+        
+        string letter ;
+
+        if (studentGrade >= 90)
+        {
+            letter = "A";
+        } else if (studentGrade >= 80)
+        {
+            letter = "B";
+        }else if (studentGrade >= 70)
+        {
+            letter = "C";
+        }else if (studentGrade >= 60)
+        {
+            letter = "D";
+        }else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is : {letter}");
+
+        if(studentGrade >= 70){
+            Console.WriteLine("Congratulations, you have passed the course!");
+        }else 
+        {
+            Console.WriteLine("Better luck next time");
+        }
     }
 }
