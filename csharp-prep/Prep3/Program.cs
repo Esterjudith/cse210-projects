485c595bf056fc93361082f414c2058a5d00751a
Judy Hanson <judy.l.hanson@gmail.com>
2024-09-20 20:14:37 -0700

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number?");
+        // string magicStringNumber = Console.ReadLine();
+        // int magicNumber = int.Parse(magicStringNumber);
+          
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+
+        int guessNumber; 
+        do
+        {
+            Console.Write("What is your guess?");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if(guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if(guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guessNumber != magicNumber );
+
+        
+        
     }
 }
