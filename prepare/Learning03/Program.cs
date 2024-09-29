08f476f44a260760e6e41720d1562f033231f240
Judy Hanson <judy.l.hanson@gmail.com>
2024-09-29 14:18:45 -0700

    Learning03 activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+       Fraction fraction = new Fraction();
+       fraction.SetTop(3);
+       fraction.SetBottom(4);
+       Fraction f = new Fraction(8);
+       Fraction frac = new Fraction(2, 7);
+       Console.WriteLine(fraction.GetFractionString());
+       Console.WriteLine(fraction.GetDecimalValue());
+       Console.WriteLine(f.GetFractionString());
+       Console.WriteLine(f.GetDecimalValue());
+       Console.WriteLine(frac.GetFractionString());
+       Console.WriteLine(frac.GetDecimalValue());
+       
     }
 }
