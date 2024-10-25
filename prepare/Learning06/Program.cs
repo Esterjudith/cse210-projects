2097fd427cd57708b102e1771a76620c353d2f50
Judy Hanson <judy.l.hanson@gmail.com>
2024-10-24 18:32:48 -0700

    Polymorphism learning activity

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        Square square = new Square("black", 10);
+        Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle("White", 3, 5);
+        Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new Circle("Red", 5);
+        Console.WriteLine(circle.GetArea());
+
+        List <Shape> shapeList = new List<Shape>();
+        shapeList.Add(circle);
+        shapeList.Add(rectangle);
+        shapeList.Add(square);  
+
+        foreach(Shape shape in shapeList)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
